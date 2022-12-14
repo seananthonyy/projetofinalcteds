436d58267170a30b4d39f2c4896af8d8dfd7eb92
thiago <tsp2911@usp.br>
2022-12-13 22:48:02 -0300

    adiciona pastas

---
+++ Interfaces/IUsuarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFteste.Models;
+
+namespace WPFteste.Interfaces
+{
+    public interface IUsuarios
+    {
+        void CriarUsuario(Usuarios novoUsuario);
+    }
+}
---
+++ Repositories/UsuariosRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFteste.Interfaces;
+using WPFteste.Models;
+
+
+
+namespace WPFteste.Repositories
+{
+    public class UsuariosRepository : IUsuarios
+    {
+        public void CriarUsuario(Usuarios novoUsuario)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "serverdefinitivo.database.windows.net";
+                builder.UserID = "thiagoantonio";
+                builder.Password = "12677679ta!";
+                builder.InitialCatalog = "dbdefinitiva";
+
+                using (SqlConnection con = new SqlConnection(builder.ConnectionString))
+                {
+                    
+                    string queryInsert = "INSERT INTO Usuarios (UsuarioNome, UsuarioSenha) VALUES (@UsuarioNome, @UsuarioSenha)";
+
+                    using (SqlCommand cmd = new SqlCommand(queryInsert, con))
+                    {
+                        cmd.Parameters.AddWithValue("@UsuarioNome", novoUsuario.Nome);
+                        cmd.Parameters.AddWithValue("@UsuarioSenha", novoUsuario.Senha);
+
+
+                        con.Open();
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
